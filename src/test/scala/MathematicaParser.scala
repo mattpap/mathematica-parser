6c04fb5c5f5c7c0cb10b585981b9674df4330226
Mateusz Paprocki <mattpap@gmail.com>
2012-07-08 03:03:12 +0200

    Implemented ~== operator for testing the parser

--- src/test/scala/MathematicaParser.scala
+++ src/test/scala/MathematicaParser.scala
@@ -1,19 +1,40 @@
 package org.sympy.tests
 
 import org.specs2.mutable.Specification
-import org.specs2.matcher.DataTables
 
 import org.sympy.{MathematicaParser,Expr,Sym,Num,Str,Eval}
+import org.sympy.{Plus,Times,Power}
+import org.sympy.MathematicaImplicits._
 
-class MathematicaParserSuite extends Specification with DataTables {
-    protected val verify = (input: String, output: Expr) =>
-        MathematicaParser.parse(input) === Some(output)
+class MathematicaParserSuite extends Specification {
+    protected class MatchString(input: String) {
+        def ~==(output: Expr) = MathematicaParser.parse(input) === Some(output)
+    }
+
+    protected implicit def stringToMatchString(value: String): MatchString = new MatchString(value)
 
     "Mathematica's language parser" should {
         "Parse constants" in {
-            "input" || "output" |>
-            "1"     !! Num("1") |
-            "2"     !! Num("2") | verify
+            "1" ~== Num("1")
+            "2" ~== Num("2")
+        }
+
+        "Parse arithmetics" in {
+            "1 + 2"         ~== Plus(1, 2)
+            "1 + 2 + 3"     ~== Plus(Plus(1, 2), 3)
+            "1 + 2 + 3 + 4" ~== Plus(Plus(Plus(1, 2), 3), 4)
+            "1 * 2"         ~== Times(1, 2)
+            "1 * 2 * 3"     ~== Times(Times(1, 2), 3)
+            "1 * 2 * 3 * 4" ~== Times(Times(Times(1, 2), 3), 4)
+            "1 + 2 * 3"     ~== Plus(1, Times(2, 3))
+            "1 + (2 * 3)"   ~== Plus(1, Times(2, 3))
+            "(1 + 2) * 3"   ~== Times(Plus(1, 2), 3)
+            "1 * 2 + 3"     ~== Plus(Times(1, 2), 3)
+            "(1 * 2) + 3"   ~== Plus(Times(1, 2), 3)
+            "1 * (2 + 3)"   ~== Times(1, Plus(2, 3))
+            "1^2"           ~== Power(1, 2)
+            // "1^2^3"         ~== Power(1, Power(2, 3))
+            // "1^2^3^4"       ~== Power(1, Power(2, Power(3, 4)))
         }
     }
 }
