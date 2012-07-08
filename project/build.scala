539e63e8b2b2e97f03b335e21345232c63ef7298
Mateusz Paprocki <mattpap@gmail.com>
2012-07-08 17:34:22 +0200

    Updated to Scala 2.10.0-M3 to get reflection support

--- project/build.scala
+++ project/build.scala
@@ -2,11 +2,14 @@ import sbt._
 import Keys._
 
 object SymPy extends Build {
+    val scalaOpts = Seq("-deprecation", "-unchecked", "-feature")
+    val languageOpts = Seq("implicitConversions")
+
     override lazy val settings = super.settings ++ Seq(
         version := "0.0.1-SNAPSHOT",
         organization := "org.sympy",
-        scalaVersion := "2.9.2",
-        scalacOptions ++= Seq("-deprecation", "-unchecked"),
+        scalaVersion := "2.10.0-M3",
+        scalacOptions ++= scalaOpts ++ languageOpts.map("-language:" + _),
         shellPrompt := { state =>
             "sympy (%s)> ".format(Project.extract(state).currentProject.id)
         },
@@ -16,7 +19,7 @@ object SymPy extends Build {
         )
     )
 
-    val specs2 = "org.specs2" %% "specs2" % "1.11" % "test"
+    val specs2 = "org.specs2" %% "specs2" % "1.9" % "test"
     val jansi = "org.fusesource.jansi" % "jansi" % "1.8"
 
     lazy val sympySettings = settings ++ Seq(
