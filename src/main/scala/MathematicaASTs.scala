52d10d6fdee029e81cac0b8ba7ea1eb5bdcd6bb9
Mateusz Paprocki <mattpap@gmail.com>
2014-09-11 19:11:16 +0200

    Add Expr.toLispForm

--- src/main/scala/MathematicaASTs.scala
+++ src/main/scala/MathematicaASTs.scala
@@ -2,31 +2,43 @@ package org.refptr.parsing.mathematica
 
 sealed trait Expr {
     def toPrettyForm: String
+    def toLispForm: String
 
     def apply(exprs: Expr*): Eval = Eval(this, exprs: _*)
 }
 
 case class Sym(name: String) extends Expr {
     def toPrettyForm = name
+    def toLispForm = '"' + name + '"'
 }
 
 case class Num(value: String) extends Expr {
     def toPrettyForm = value
+    def toLispForm = value
 }
 
 case class Str(value: String) extends Expr {
-    def toPrettyForm = "\"%s\"".format(value.replace("\"", "\\\\\""))
+    def toPrettyForm = '"' + value.replace("\"", "\\\\\"") + '"'
+    def toLispForm = toPrettyForm
 }
 
 case class Eval(head: Expr, args: Expr*) extends Expr {
+    private def toForm(fn: Expr => String): (String, String) = {
+        (fn(head), args.map(fn).mkString(", "))
+    }
+
     def toPrettyForm = {
-        val head = this.head.toPrettyForm
-        val args = this.args.map(_.toPrettyForm).mkString(", ")
+        val (head, args) = toForm(_.toPrettyForm)
         s"$head[$args]"
     }
 
-    override def toString: String = {
-        val args = this.args.map(_.toString).mkString(", ")
+    def toLispForm = {
+        val (head, args) = toForm(_.toLispForm)
+        s"[$head, $args]"
+    }
+
+    override def toString = {
+        val (head, args) = toForm(_.toString)
         s"Eval($head, $args)"
     }
 }
